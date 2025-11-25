0d00cbc4827d0f2839f5a26b4a0c9ebcf498c719
javid <javiddeveloper@gmail,com>
2025-11-25 22:53:35 +0330

    refactor: Koin and database setup on Android

---
+++ composeApp/src/androidMain/kotlin/xyz/sattar/javid/proqueue/di/KoinModules.android.kt
@@ -0,0 +1,16 @@
+package xyz.sattar.javid.proqueue.di
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import xyz.sattar.javid.proqueue.data.localDataSource.AppDatabase
+import xyz.sattar.javid.proqueue.db.DbFactoryAndroid
+
+val dbModuleAndroid = module {
+
+    // Ensure that the database is initialized before using it
+    single<AppDatabase> {
+        DbFactoryAndroid.initDatabase(context = androidContext())
+        DbFactoryAndroid.initialize()
+    }
+
+}
