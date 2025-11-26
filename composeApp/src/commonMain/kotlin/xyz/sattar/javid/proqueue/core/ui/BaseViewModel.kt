1428a478ad5a0e2cea0bd804c3bfafeedb768eb2
javid <javiddeveloper@gmail,com>
2025-11-26 23:06:42 +0330

    refactor: Remove SavedStateHandle from BaseViewModel

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/core/ui/BaseViewModel.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/core/ui/BaseViewModel.kt
@@ -1,32 +1,29 @@
 package xyz.sattar.javid.proqueue.core.ui
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.launch
-private const val SAVED_UI_STATE_KEY = "savedUiStateKey"
 
-@ExperimentalCoroutinesApi
-abstract class BaseViewModel<STATE , PARTIAL_STATE, EVENT, INTENT>(
-    savedStateHandle: SavedStateHandle,
+abstract class BaseViewModel<STATE, PARTIAL_STATE, EVENT, INTENT>(
     initialState: STATE
 ) : ViewModel() {
 
     private val intentChannel = Channel<INTENT>(Channel.UNLIMITED)
 
-    private val _uiState = savedStateHandle.getStateFlow(SAVED_UI_STATE_KEY, initialState)
-    val uiState: StateFlow<STATE> = _uiState
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState: StateFlow<STATE> = _uiState.asStateFlow()
 
     private val eventChannel = Channel<EVENT>(Channel.BUFFERED)
     val events = eventChannel.receiveAsFlow()
@@ -43,7 +40,7 @@ abstract class BaseViewModel<STATE , PARTIAL_STATE, EVENT, INTENT>(
                 .scan(uiState.value) { currentState, partialState ->
                     reduceState(currentState, partialState)
                 }.collect { newState ->
-                    savedStateHandle[SAVED_UI_STATE_KEY] = newState
+                    _uiState.value = newState
                 }
         }
     }
@@ -62,7 +59,7 @@ abstract class BaseViewModel<STATE , PARTIAL_STATE, EVENT, INTENT>(
     }
 
     protected fun doAsyncTask(task: suspend () -> Unit): Flow<PARTIAL_STATE> {
-        viewModelScope.launch((Dispatchers.IO)) {
+        viewModelScope.launch(Dispatchers.IO) {
             task.invoke()
         }
         return emptyFlow<PARTIAL_STATE>()
