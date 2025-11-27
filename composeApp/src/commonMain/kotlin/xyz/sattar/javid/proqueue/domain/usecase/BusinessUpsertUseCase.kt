925804973acf019bfdf7ec61ad246fe28cb6c614
javid <javiddeveloper@gmail,com>
2025-11-27 15:53:43 +0330

    feat: Set creation timestamp on business upsert

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/domain/usecase/BusinessUpsertUseCase.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/domain/usecase/BusinessUpsertUseCase.kt
@@ -2,8 +2,14 @@ package xyz.sattar.javid.proqueue.domain.usecase
 
 import xyz.sattar.javid.proqueue.domain.BusinessRepository
 import xyz.sattar.javid.proqueue.domain.model.Business
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 
 class BusinessUpsertUseCase(private val repository: BusinessRepository) {
-    suspend operator fun invoke(business: Business): Boolean = repository.upsertBusiness(business)
+    @OptIn(ExperimentalTime::class)
+    suspend operator fun invoke(business: Business): Boolean =
+        repository.upsertBusiness(business.copy(
+            createTimeStamp = Clock.System.now().toEpochMilliseconds())
+        )
 }
