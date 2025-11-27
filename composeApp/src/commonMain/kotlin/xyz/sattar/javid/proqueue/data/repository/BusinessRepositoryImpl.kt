7f5d40e6968da6e8e0aecbfe5c093b94691ead60
javid <javiddeveloper@gmail,com>
2025-11-27 15:52:43 +0330

    feat: Implement BusinessRepository logic

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/data/repository/BusinessRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/data/repository/BusinessRepositoryImpl.kt
@@ -1,19 +1,39 @@
 package xyz.sattar.javid.proqueue.data.repository
 
 import xyz.sattar.javid.proqueue.data.localDataSource.dao.BusinessDao
+import xyz.sattar.javid.proqueue.data.localDataSource.mapper.toDomain
+import xyz.sattar.javid.proqueue.data.localDataSource.mapper.toEntity
 import xyz.sattar.javid.proqueue.domain.BusinessRepository
 import xyz.sattar.javid.proqueue.domain.model.Business
 import xyz.sattar.javid.proqueue.domain.model.Visitor
 
 class BusinessRepositoryImpl(
     private val dao: BusinessDao
-): BusinessRepository {
-    override fun upsertVisitor(visitor: Visitor): Boolean {
-        TODO("Not yet implemented")
+) : BusinessRepository {
+    override suspend fun upsertVisitor(visitor: Visitor): Boolean {
+        return try {
+            dao.upsertVisitor(visitor.toEntity())
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
-    override fun upsertBusiness(business: Business): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun upsertBusiness(business: Business): Boolean {
+        return try {
+            dao.upsertBusiness(business.toEntity())
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override suspend fun loadLastBusiness(): Business? {
+        return try {
+            dao.loadLastBusiness().toDomain()
+        } catch (e: Exception) {
+            null
+        }
     }
 
 
