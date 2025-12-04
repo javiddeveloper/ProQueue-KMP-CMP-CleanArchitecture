10ea00ce1c2b44b17a1e42c385489ef34287cfa4
javid <javiddeveloper@gmail,com>
2025-12-04 17:29:57 +0330

    feat: Enhance BusinessDao functionality

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/data/localDataSource/dao/BusinessDao.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/data/localDataSource/dao/BusinessDao.kt
@@ -2,6 +2,7 @@ package xyz.sattar.javid.proqueue.data.localDataSource.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import xyz.sattar.javid.proqueue.data.localDataSource.entity.BusinessEntity
 import xyz.sattar.javid.proqueue.data.localDataSource.entity.VisitorEntity
@@ -15,14 +16,26 @@ interface BusinessDao {
     @Upsert
     suspend fun upsertBusiness(business: BusinessEntity)
 
-    @Query("SELECT * FROM Business  ORDER BY createTimeStamp DESC limit 1")
-    suspend fun loadLastBusiness(): BusinessEntity
+    @Query("SELECT * FROM Business ORDER BY createTimeStamp DESC")
+    suspend fun loadAllBusiness(): List<BusinessEntity>
 
     @Query("SELECT * FROM Visitor Where statusInQueue=0 ORDER BY updateTimeStamp DESC")
     suspend fun getWaitingVisitor(): List<VisitorEntity>
 
     @Query("DELETE FROM visitor where id=:visitorId")
     suspend fun deleteVisitor(visitorId:Int)
+
+    @Query("DELETE FROM Business WHERE id = :businessId")
+    suspend fun deleteBusiness(businessId: Int)
+
+    @Query("DELETE FROM Visitor")
+    suspend fun deleteAllVisitors()
+
+    @Transaction
+    suspend fun deleteBusinessAndQueue(businessId: Int) {
+        deleteBusiness(businessId)
+    }
+
 
 }
 
