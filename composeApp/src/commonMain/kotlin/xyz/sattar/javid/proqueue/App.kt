83ff81035869bd4e7df8ffc97f360975af201c51
javid <javiddeveloper@gmail,com>
2025-12-04 15:08:32 +0330

    feat: Add MainScreen as the entry point

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/App.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/App.kt
@@ -1,35 +1,21 @@
 package xyz.sattar.javid.proqueue
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import xyz.sattar.javid.proqueue.feature.createBusiness.CreateBusinessRoute
+import xyz.sattar.javid.proqueue.feature.main.MainScreen
 import xyz.sattar.javid.proqueue.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-
     AppTheme {
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                CreateBusinessRoute(onContinue = {}, onNavigateBack = {})
-            }
-        }
-    }
+        MainScreen(
+            onNavigateToCreateBusiness = {
 
+            },
+            onNavigateToCreateVisitor = {
+
+            }
+        )
+    }
 }
