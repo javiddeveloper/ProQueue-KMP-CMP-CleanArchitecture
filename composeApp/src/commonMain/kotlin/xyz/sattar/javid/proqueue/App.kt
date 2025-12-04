8e28ce98329b4138b6ffc55db3575390c63c3f5a
javid <javiddeveloper@gmail,com>
2025-12-04 17:32:51 +0330

    feat: Implement navigation based on business selection

--- composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/App.kt
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/App.kt
@@ -1,21 +1,40 @@
 package xyz.sattar.javid.proqueue
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import xyz.sattar.javid.proqueue.feature.main.MainScreen
+import xyz.sattar.javid.proqueue.core.state.BusinessStateHolder
+import xyz.sattar.javid.proqueue.core.navigation.navHost.BusinessNavHost
+import xyz.sattar.javid.proqueue.core.navigation.navHost.MainNavHost
 import xyz.sattar.javid.proqueue.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        MainScreen(
-            onNavigateToCreateBusiness = {
+        val selectedBusiness by BusinessStateHolder.selectedBusiness.collectAsState()
 
-            },
-            onNavigateToCreateVisitor = {
-
-            }
-        )
+        if (selectedBusiness == null) {
+            BusinessNavHost(
+                onBusinessSelected = { business ->
+                    BusinessStateHolder.selectBusiness(business)
+                }
+            )
+        } else {
+            MainNavHost(
+                onNavigateToCreateBusiness = {
+                    // Should not be reachable or should navigate to business list?
+                    // For now, let's just clear business to go back to list
+                    BusinessStateHolder.clearBusiness()
+                },
+                onNavigateToCreateVisitor = {
+                    // TODO: Implement navigation to CreateVisitor
+                },
+                onChangeBusiness = {
+                    BusinessStateHolder.clearBusiness()
+                }
+            )
+        }
     }
 }
