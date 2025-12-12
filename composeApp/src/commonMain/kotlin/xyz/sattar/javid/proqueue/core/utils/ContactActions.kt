95ee2bef968d00b9ad4d11beac3c882ae73d1d16
javid <javiddeveloper@gmail,com>
2025-12-12 12:06:45 +0330

    feat: Add contact action handlers

---
+++ composeApp/src/commonMain/kotlin/xyz/sattar/javid/proqueue/core/utils/ContactActions.kt
@@ -0,0 +1,7 @@
+package xyz.sattar.javid.proqueue.core.utils
+
+expect fun openSms(phone: String)
+expect fun openWhatsApp(phone: String)
+expect fun openTelegram(phone: String)
+expect fun openPhoneDial(phone: String)
+
