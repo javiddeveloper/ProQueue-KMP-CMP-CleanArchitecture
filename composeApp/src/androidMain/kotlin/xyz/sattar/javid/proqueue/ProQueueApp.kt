35606ba0866dadabc241db220108414513079843
javid <javiddeveloper@gmail,com>
2025-12-12 12:10:04 +0330

    feat: Implement Android contact actions

--- composeApp/src/androidMain/kotlin/xyz/sattar/javid/proqueue/ProQueueApp.kt
+++ composeApp/src/androidMain/kotlin/xyz/sattar/javid/proqueue/ProQueueApp.kt
@@ -1,15 +1,20 @@
 package xyz.sattar.javid.proqueue
 
 import android.app.Application
+import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import xyz.sattar.javid.proqueue.di.appModule
 import xyz.sattar.javid.proqueue.di.dbModuleAndroid
 
 class ProQueueApp : Application() {
+    companion object {
+        lateinit var appContext: Context
+    }
 
     override fun onCreate() {
         super.onCreate()
+        appContext = applicationContext
         startKoin {
             androidContext(applicationContext)
             modules(dbModuleAndroid, appModule)
---
+++ composeApp/src/androidMain/kotlin/xyz/sattar/javid/proqueue/core/utils/ContactActions.android.kt
@@ -0,0 +1,30 @@
+package xyz.sattar.javid.proqueue.core.utils
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.ui.platform.LocalContext
+
+private fun launchUri(uri: String) {
+    val context = xyz.sattar.javid.proqueue.ProQueueApp.appContext
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri)).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+}
+
+actual fun openSms(phone: String) {
+    launchUri("sms:$phone")
+}
+
+actual fun openWhatsApp(phone: String) {
+    launchUri("https://wa.me/$phone")
+}
+
+actual fun openTelegram(phone: String) {
+    launchUri("https://t.me/$phone")
+}
+
+actual fun openPhoneDial(phone: String) {
+    val context = xyz.sattar.javid.proqueue.ProQueueApp.appContext
+    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+}
+
